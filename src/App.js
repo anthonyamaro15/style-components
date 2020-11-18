cf403e1d153e7d42e910c9910629959a0a710aff
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-11-18 10:39:46 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,89 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useCallback } from "react";
+import  GoogleLogin  from 'react-google-login';
+ import FacebookLogin from 'react-facebook-login-typed';
+import { FaFacebookSquare } from "react-icons/fa";
+import { ImFacebook2 } from "react-icons/im";
 
-import Button from './components/Button';
+// desctructuring Props
+ const App = () => {
+  const [increment, setIncrement] = useState(0);
+ 
 
 
+  const add = () => {
+    setIncrement(increment + 1);
+  };
+
+  // this one for google api
+  const responseGoogle = (response) => {
+     console.log("what is response?? ", response);
+  }
+
+  const onErrorHere = (response) => {
+     console.log('what is the error', response);
+  }
+
+   const responseFacebook = useCallback((response) => {
+      console.log('this was click')
+      if(response.status !== "unknown") {
+         console.log('successfull call', response)
+      }
+   },[])
 
 
-function App() {
+   // define this to use facebook logout
+   // window.fbAsyncInit = () => {
+   //    window.FB.init({
+   //       appId: "APPIDHERE",
+   //       cookie: true,
+   //       xfbml: true,
+         
+   //    })
+   // }
+
+   // call this function to logout
+   // const facebookLogout = () => {
+   //    window.FB.logout(() => {
+   //       console.log('person logged out');
+   //    })
+   // }
+
+   
+
   return (
-    <div >
-   {/**
-      passing props make sense but still a bit confuse how primary works without a value
-   */}
-     <Button primary bgColor="purple">Login</Button>
-    
+    <div>
+        <GoogleLogin
+    clientId={`${process.env.REACT_APP_API_GOOGLE_ID}`}
+    buttonText="Login with google"
+    onSuccess={responseGoogle}
+    onFailure={onErrorHere}
+    isSignedIn={true}
+    cookiePolicy={'single_host_origin'}
+    render={(renderProps) => (
+       <button onClick={renderProps.onClick}>custom google button</button>
+    )}
+  />
+      <FacebookLogin 
+      appId={`${process.env.REACT_APP_API_FACEBOOK_ID}`}
+      autoLoad={false}
+      // fields="name,email,picture"
+      callback={responseFacebook}
+      
+      render={(renderProps) => (
+         <button className="facebook"  onClick={renderProps.onClick}>facebook custom button</button>
+      )}
+    />
+
+    <span className="icon">
+ <FaFacebookSquare />
+
+    </span>
+   <ImFacebook2 />
+      <input type="text" />
+      <p>{`count starts at: ${increment}`}</p>
+      <button onClick={add}>add</button>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -23,19 +23,16 @@ const StyledButton = styled.button`
 `
 
 // whenever you want to another button with same properties but with maybe a different color
-const DifferentButton = styled(StyledButton)`
-   font-size: 3rem;
-   color: green;
-`
+// const DifferentButton = styled(StyledButton)`
+//    font-size: 3rem;
+//    color: green;
+// `
 
 const Button = ({children, primary, bgColor}) => {
-
-
    return (
       // properties are coming from the App file
       <>
       <StyledButton primary={primary} bgColor={bgColor}>{children}</StyledButton>
-      <DifferentButton >{children}</DifferentButton>
       </>
    )
    
